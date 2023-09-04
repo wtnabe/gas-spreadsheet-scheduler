b60c93740ada72349c2c639dc9c80321294b88d9
wtnabe <18510+wtnabe@users.noreply.github.com>
2023-09-05 08:03:48 +0900

    feat: add month features and inDuration()
     * add thatWeek(), thatMonth() for get the week/month to which any day belongs
     * add thisMonth(), lastMonth(), nextMonth()
     * add inDuration() for given date is within a specific period

--- spec/date-utils_spec.js
+++ spec/date-utils_spec.js
@@ -1,11 +1,10 @@
-/* global describe, it, before */
+/* global describe, it, beforeEach, afterEach */
 
 import assert from 'power-assert'
 import sinon from 'sinon'
 import gas from 'gas-local'
-import { addDays } from 'date-fns'
 
-import { parseDateWithLocalTZ } from './support/utils.js'
+import { parseDateWithLocalTZ as date } from './support/utils.js'
 
 const app = gas.require('src', {
   console
@@ -15,20 +14,120 @@ describe('DateUtils', () => {
   let dateUtils
 
   function today () {
-    return parseDateWithLocalTZ('2023-05-27')
+    return date('2023-05-27')
   }
 
-  before(() => {
-    dateUtils = app.createDateUtils()
-    sinon.stub(dateUtils, 'today').returns(today())
+  afterEach(() => {
+    sinon.restore()
   })
 
-  describe('#thisWeek', () => {
-    it('return sunday ( getDay() is 0 ) and next sunday', () => {
-      assert.deepEqual(
-        dateUtils.thisWeek(),
-        [parseDateWithLocalTZ('2023-05-21'), parseDateWithLocalTZ('2023-05-28')]
-      )
+  describe('week', () => {
+    beforeEach(() => {
+      dateUtils = app.createDateUtils()
+      sinon.stub(dateUtils, 'today').returns(today())
+    })
+
+    describe('#thatWeek', () => {
+      it('2023-05-05 is the week of 2023-04-30 to 2023-05-06', () => {
+        assert.deepEqual(
+          dateUtils.thatWeek(date('2023-05-05')),
+          [ date('2023-04-30'), date('2023-05-07') ]
+        )
+      })
+    })
+
+    describe('#lastWeek', () => {
+      it('return last of last Sunday and last Sunday', () => {
+        assert.deepEqual(
+          dateUtils.lastWeek(),
+          [ date('2023-05-14'), date('2023-05-21') ]
+        )
+      })
+    })
+
+    describe('#thisWeek', () => {
+      it('return sunday ( getDay() is 0 ) and next sunday', () => {
+        assert.deepEqual(
+          dateUtils.thisWeek(),
+          [date('2023-05-21'), date('2023-05-28')]
+        )
+      })
+    })
+
+    describe('#nextWeek', () => {
+      it('return next sunday and next of next sunday ( even across months )', () => {
+        assert.deepEqual(
+          dateUtils.nextWeek(),
+          [ date('2023-05-28'), date('2023-06-04') ]
+        )
+      })
+    })
+  })
+
+  describe('month', () => {
+    describe('#thatMonth', () => {
+      it('', () => {
+        assert.deepEqual(
+          dateUtils.thatMonth(date('2023-06-11')),
+          [ date('2023-06-01'), date('2023-07-01') ]
+        )
+      })
+    })
+
+    describe('#lastMonth', () => {
+      beforeEach(() => {
+        sinon.stub(dateUtils, 'today').returns(date('2023-01-15'))
+      })
+      it('return from 1st day of last month ( even across years )', () => {
+        assert.deepEqual(
+          dateUtils.lastMonth(),
+          [ date('2022-12-01'), date('2023-01-01') ]
+        )
+      })
+    })
+
+    describe('#thisMonth', () => {
+      beforeEach(() => {
+        sinon.stub(dateUtils, 'today').returns(date('2023-05-27'))
+      })
+      it('return from 1st day to next 1st day', () => {
+        assert.deepEqual(
+          dateUtils.thisMonth(),
+          [date('2023-05-01'), date('2023-06-01')]
+        )
+      })
+    })
+
+    describe('#nextMonth', () => {
+      beforeEach(() => {
+        sinon.stub(dateUtils, 'today').returns(date('2023-12-11'))
+      })
+      it('from next 1st day to next after next 1st day ( even across years )', () => {
+        assert.deepEqual(
+          dateUtils.nextMonth(),
+          [ date('2024-01-01'), date('2024-02-01') ]
+        )
+      })
+    })
+  })
+
+  describe('#inDuration', () => {
+    describe('#inThisWeek', () => {
+      it('2023-05-27 is in this week', () => {
+        assert(dateUtils.inThisWeek(dateUtils.today()))
+      })
+      it('2023-05-28 is not this week', () => {
+        assert(dateUtils.inThisWeek(date('2023-05-28')) === false)
+      })
+    })
+
+    describe('#inThisMonth', () => {
+      it('2023-05-27 is in this month', () => {
+        assert(dateUtils.inThisMonth(dateUtils.today()))
+      })
+      it('2023-06-03 is not this month', () => {
+        assert(dateUtils.inThisMonth(date('2023-06-03')) === false)
+      })
     })
   })
 })
--- src/date-utils.js
+++ src/date-utils.js
@@ -15,6 +15,21 @@ class DateUtils {
   }
 
   /**
+   * @param {Date} date
+   * @returns {Array<Date>}
+   */
+  thatWeek (date) {
+    const dow = date.getDay()
+
+    const from = new Date(date.getTime())
+    from.setDate(date.getDate() - dow)
+    const to = new Date(date.getTime())
+    to.setDate(date.getDate() + (7 - dow))
+
+    return [from, to]
+  }
+
+  /**
    * @returns {Array<Date>}
    */
   lastWeek () { // eslint-disable-line no-unused-vars
@@ -32,15 +47,7 @@ class DateUtils {
    * @returns {Array<Date>}
    */
   thisWeek () {
-    const day = this.today()
-    const dow = day.getDay()
-
-    const from = new Date(day.getTime())
-    from.setDate(day.getDate() - dow)
-    const to = new Date(day.getTime())
-    to.setDate(day.getDate() + (7 - dow))
-
-    return [from, to]
+    return this.thatWeek(this.today())
   }
 
   /**
@@ -53,6 +60,132 @@ class DateUtils {
     to.setDate(to.getDate() + 7)
 
     return [from, to]
+  }
+
+  /**
+   * from 1st day of month to next 1st day of month includes date
+   *
+   * @param {Date} date
+   * @returns {Array<Date>}
+   */
+  thatMonth (date) {
+    const from = new Date(date.getTime())
+    from.setDate(1)
+    const to = new Date(date.getTime())
+    to.setMonth(to.getMonth() + 1)
+    to.setDate(1)
+
+    return [from, to]
+  }
+
+  /**
+   * @returns {Array<Date>}
+   */
+  lastMonth () {
+    const [from, to] = this.thisMonth()
+
+    from.setMonth(from.getMonth() - 1)
+    to.setMonth(to.getMonth() - 1)
+
+    return [from, to]
+  }
+
+  /**
+   * from 1st day of month to next 1st day of month
+   *
+   * @returns {Array<Date>}
+   */
+  thisMonth () {
+    const [from, to] = this.thatMonth(this.today())
+
+    return [from, to]
+  }
+
+  /**
+   * @returns {Array<Date>}
+   */
+  nextMonth () {
+    const [from, to] = this.thisMonth()
+
+    from.setMonth(from.getMonth() + 1)
+    to.setMonth(to.getMonth() + 1)
+
+    return [from, to]
+  }
+
+  /**
+   * from 1st day of month to next 1st day of month
+   *
+   * @returns {Array<Date>}
+   */
+  /**
+   * @param {Date} date
+   * @param {Date} from
+   * @param {Date} to
+   * @returns {boolean}
+   */
+  inDuration (date, from, to) {
+    return from <= date && date < to
+  }
+
+  /**
+   * @param {Date} date
+   * @returns {boolean}
+   */
+  inLastWeek (date) {
+    const [from, to] = this.lastWeek()
+
+    return this.inDuration(date, from, to)
+  }
+
+  /**
+   * @param {Date} date
+   * @returns {boolean}
+   */
+  inThisWeek (date) {
+    const [from, to] = this.thisWeek()
+
+    return this.inDuration(date, from, to)
+  }
+
+  /**
+   * @param {Date} date
+   * @returns {boolean}
+   */
+  inNextWeek (date) {
+    const [from, to] = this.nextWeek()
+
+    return this.inDuration(date, from, to)
+  }
+
+  /**
+   * @param {Date} date
+   * @returns {boolean}
+   */
+  inLastMonth (date) {
+    const [from, to] = this.lastMonth()
+
+    return this.inDuration(date, from, to)
+  }
+
+  /**
+   * @param {Date} date
+   * @returns {boolean}
+   */
+  inThisMonth (date) {
+    const [from, to] = this.thisMonth()
+
+    return this.inDuration(date, from, to)
+  }
+
+  /**
+   * @param {Date} date
+   * @returns {boolean}
+   */
+  inNextMonth (date) {
+    const [from, to] = this.nextMonth()
+
+    return this.inDuration(date, from, to)
   }
 }
 
